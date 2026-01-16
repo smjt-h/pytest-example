c5758c68db9426f1d7086bb3ac85a302f951b7fb
Soumyajit Das <soumyajit.das@harness.io>
2026-01-16 15:11:22 +0530

    Update test_additions.py

--- tests/mymodule/test_additions.py
+++ tests/mymodule/test_additions.py
@@ -2,7 +2,7 @@ import pytest
 
 from myapp.mymodule.funcs import add
 
-# jhij
+# jhijsa
 @pytest.mark.parametrize("a, b, c", [(10,20, 30), (20,40,60), (11,22,33)])
 def test_add(a, b, c):
     res = add(a, b)
